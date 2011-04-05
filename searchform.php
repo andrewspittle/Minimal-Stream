987e5aa9f05ae33f815fd677156acfb20108bb0a
Andrew Spittle <andsp13@gmail.com>
2011-04-04 21:34:09 -0700

    Better searchform style.

--- searchform.php
+++ searchform.php
@@ -1,6 +1,3 @@
-<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-    <div>
-        <input type="text" value="" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="Search" />
-    </div>
+<form role="search" id="searchform" method="get" action="<?php echo home_url( '/' ); ?>">
+	<input type="text" name="s" id="s" value="Type and press enter..." onfocus="if (this.value == 'Type and press enter...') {this.value = '';}" onblur="if (this.value == '') {this.value = 'Type and press enter...';}" />
 </form>
