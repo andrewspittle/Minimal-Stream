0852eb55e563c7080eebdcb69b0b15573834f452
Andrew Spittle <andrew@andrewspittle.net>
2011-05-27 16:14:35 -0700

    New footer template.

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
 <div id="footer">
+	<h4 id="archives-head">Archives</h4>
+	<div id="archives-wrap">
+		<?php ms_archives_column(); ?>
+	</div>
 	<?php get_search_form(); ?>
 </div><!-- #footer -->
 </div><!-- #container -->
