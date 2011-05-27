68293a4f93c1b6f153e550c632ddf0e186e57e40
Andrew Spittle <andrew@andrewspittle.net>
2011-05-27 16:15:35 -0700

    New loop without bottom navigation.

--- loop.php
+++ loop.php
@@ -120,11 +120,3 @@
 	<?php endif; // This was the if statement that broke the loop into three parts based on categories. ?>
 
 <?php endwhile; // End the loop. Whew. ?>
-
-<?php /* Display navigation to next/previous pages when applicable */ ?>
-<?php if (  $wp_query->max_num_pages > 1 ) : ?>
-				<div id="nav-below" class="navigation">
-					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'minimalstream' ) ); ?></div>
-					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'minimalstream' ) ); ?></div>
-				</div><!-- #nav-below -->
-<?php endif; ?>
