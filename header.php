ef96a3df4eb0ec48e514834a1c1aa900eded564b
Andrew Spittle <andrew@andrewspittle.net>
2012-10-20 17:21:12 -0700

    Remove Mint tag from header.

--- header.php
+++ header.php
@@ -38,7 +38,6 @@
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
-<script src="http://andsp.com/mint/?js" type="text/javascript"></script>
 <?php wp_head(); ?>
 </head>
 
